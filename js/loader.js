ac793b1331e85c40e5ed38a68129f3cc0a5eb98e
Diano <diano.faniry@gmail.com>
2025-08-06 22:22:30 +0400

    2000 ms

--- js/loader.js
+++ js/loader.js
@@ -2,6 +2,6 @@ window.addEventListener("load", () => {
     const loader = document.querySelector(".loader-wrapper");
     setTimeout(() => {
       loader.classList.add("hidden");
-    }, 800); // minimum 800ms affichage du loader
+    }, 2000); // minimum 800ms affichage du loader
   });
   
